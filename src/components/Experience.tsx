5e8f232e51fc75323ad94217ea8c5d82a6d7b90a
Sanket Karki <114918951+Sanket2060@users.noreply.github.com>
2025-09-05 08:15:32 +0545

    added descriptions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,16 +6,6 @@ const Experience = () => {
         <div className="text-[#353334] mx-auto text-center">EXPERIENCE</div>
       </div>
       <div className="mt-6 companies xl:w-[900px]">
-      <div className="company1 ml-5 mb-10">
-          <div className="name text-white text-xl font-bold">
-            HirefinnAI
-          </div>
-          <div className="description text-[#998f8f] mt-3 pr-8">
-            Fullstack Developer, contributing to development of scalable and efficient web
-            applications. Collaborated with teams to design user-friendly
-            interfaces and implement backend solutions.
-          </div>
-        </div>
         <div className="company1 ml-5">
           <div className="name text-white text-xl font-bold">
             Dezvu Technologies
@@ -25,7 +15,7 @@ const Experience = () => {
             applications. Collaborated with teams to design user-friendly
             interfaces and implement backend solutions.
           </div>
-          <div className="time text-[#998f8f] mt-3">Sept 2024-Dec 2024</div>
+          <div className="time text-[#998f8f] mt-3">Oct 2024-Dec 2024</div>
         </div>
       </div>
     </div>
